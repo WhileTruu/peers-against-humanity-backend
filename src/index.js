e178eb625be1dd1a6ec78e2dc7167f9340599450
WhileTruu <marten.truu@gmail.com>
2017-03-10 14:20:49 +0200

    make backend stop serving static stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { json } from 'body-parser'
 import http from 'http'
-import path from 'path'
 
 import logger, { loggingMiddleware } from './logger'
 import controller from './api'
@@ -19,10 +18,6 @@ const app = express()
 app.use(json())
 app.use(loggingMiddleware())
 app.use('/api/v1', controller)
-
-app.use('/', express.static(path.join(__dirname, '../build')))
-app.use('/cards/*', express.static(path.join(__dirname, '../build')))
-app.use('/users/*', express.static(path.join(__dirname, '../build')))
 
 const server = http.createServer(app)
 server.listen(PORT, () => logger.info(`Server running at ${PORT}`))
